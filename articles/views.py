4976514483947f8fb80023c008e245fa12304afe
Oleg Gordienko <admin@gordio.pp.ua>
2014-03-12 17:04:59 +0200

    articles: Show articles lower than pub_date

--- articles/views.py
+++ articles/views.py
@@ -1,13 +1,28 @@
 from django.views.generic import ListView, DetailView
 from .models import Article
+from datetime import datetime
 
 
 class ArticlesListView(ListView):
-    model = Article
     context_object_name = 'articles'
     paginate_by = 8
 
+    def get_queryset(self):
+        """
+        Return Articles greater than pub_date
+        """
+        return Article.objects.filter(
+            pub_date__lte=datetime.now()
+        ).order_by('-pub_date')
+
 
 class ArticleDetailView(DetailView):
-    model = Article
     context_object_name = 'article'
+
+    def get_queryset(self):
+        """
+        Return Articles greater than pub_date
+        """
+        return Article.objects.filter(
+            pub_date__lte=datetime.now()
+        ).order_by('-pub_date')
